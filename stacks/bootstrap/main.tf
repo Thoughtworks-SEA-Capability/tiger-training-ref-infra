757f3bc8350f1462722332ab4ecc0f71d958b60b
Ankit Wal <ankitwal@thoughtworks.com>
2022-08-25 11:47:45 +0800

    Add IAM user for CircleCI

--- stacks/bootstrap/main.tf
+++ stacks/bootstrap/main.tf
@@ -45,3 +45,33 @@ resource "aws_ecr_repository" "sample-ap-a" {
 resource "aws_ecr_repository" "test-pod" {
   name = "infra-training-test-pod"
 }
+
+# IAM User for Circle CI
+# Ideally we should set this up with role + federated web identity using github tokens.
+resource "aws_iam_user" "circle_ci_admin" {
+  name               = "circle-ci-admin"
+  tags = local.tags
+}
+
+# Needs to be created manually
+#resource "aws_iam_access_key" "github-push-to-ecr" {
+#  user = aws_iam_user.github-push-to-ecr.name
+#}
+
+data "aws_iam_policy_document" "circle_ci_admin"{
+  version = "2012-10-17"
+  statement {
+    actions = [
+      "*",
+    ]
+    resources = ["*"]
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_user_policy" "circle_ci_admin" {
+  name = "circle-ci-agent"
+  policy = data.aws_iam_policy_document.circle_ci_admin.json
+  user = aws_iam_user.circle_ci_admin.name
+}
+
