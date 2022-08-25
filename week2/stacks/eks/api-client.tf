3cc952913834a98940074c3c2594a7d7144d8413
Ankit Wal <ankitwal@thoughtworks.com>
2022-08-25 18:50:23 +0800

    Add eks nodes v2 read to eks api-client

--- week2/stacks/eks/api-client.tf
+++ week2/stacks/eks/api-client.tf
@@ -7,13 +7,12 @@ data "aws_ssm_parameter" "eks_master_subnets" {
 
 data "aws_ssm_parameter" "eks_node_subnets" {
   name = "${var.team_name}-${var.environment}-eks-node-subnets"
-  lifecycle {
-    postcondition {
-      condition     = can(toset(jsondecode(self.value)))
-      error_message = "The value should be a set of subnet ids"
-    }
-  }
 }
+
+data "aws_ssm_parameter" "eks_node_subnets_v2" {
+  name = "${var.team_name}-${var.environment}-eks-node-subnets_v2"
+}
+
 data "aws_ssm_parameter" "vpc_id" {
   name = "${local.dependency_prefix}-vpc-id"
 }
@@ -21,6 +20,7 @@ data "aws_ssm_parameter" "vpc_id" {
 locals {
   eks_master_subnets = toset(jsondecode(data.aws_ssm_parameter.eks_master_subnets.value))
   eks_node_subnets = toset(jsondecode(data.aws_ssm_parameter.eks_node_subnets.value))
+  eks_node_subnets_v2 = toset(jsondecode(data.aws_ssm_parameter.eks_node_subnets_v2.value))
   vpc_id = tostring(data.aws_ssm_parameter.vpc_id.value)
 }
 
