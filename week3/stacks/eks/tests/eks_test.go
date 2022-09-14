4b1f7371ae08fed190f806483703c10f18725934
Ankit Wal <ankitwal@thoughtworks.com>
2022-09-14 18:36:15 +0800

    Refactor eks tests

--- week3/stacks/eks/tests/eks_test.go
+++ week3/stacks/eks/tests/eks_test.go
@@ -43,26 +43,14 @@ func TestEKSStack(t *testing.T) {
 		defer k8s.DeleteNamespace(t, options, namespaceName)
 	})
 
-	t.Run("ShouldHaveRequiredNameSpace", func(t *testing.T) {
-		// This is an example of a useless test - it only tests that the namespace exists, we should not expect the
-		// stack to be able to apply successfully without the namespace existing, given the declarative nature of terraform
-	})
-
 	t.Run("ShouldBeAbleToCreatePodInApplicationNamespace", func(t *testing.T) {
 		options := k8s.NewKubectlOptions("", "", namespace)
 		k8s.KubectlApply(t, options, "./pod.yml")
 		defer k8s.KubectlDelete(t, options, "./pod.yml")
 		k8s.WaitUntilPodAvailable(t, options, "test-eks-pod", 15, 3*time.Second)
+
+		t.Run("PodsShouldBeAbleToTalkToPublicEndpoints", func(t *testing.T) {
+			k8s.RunKubectl(t, options, []string{"exec", "test-eks-pod", "--", "sh", "-c", "curl www.google.com"}...)
+		})
 	})
-
-	t.Run("PodsShouldBeAbleToTalkToPublicEndpoints", func(t *testing.T) {
-		options := k8s.NewKubectlOptions("", "", namespace)
-		k8s.KubectlApply(t, options, "./pod.yml")
-		defer k8s.KubectlDelete(t, options, "./pod.yml")
-		k8s.WaitUntilPodAvailable(t, options, "test-eks-pod", 15, 3*time.Second)
-
-		k8s.RunKubectl(t, options, []string{"exec", "test-eks-pod", "--", "sh", "-c", "curl www.google.com"}...)
-
-	})
-
 }
