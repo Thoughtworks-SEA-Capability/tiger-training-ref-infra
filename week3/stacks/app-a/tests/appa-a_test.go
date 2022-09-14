190b096a0b4258c0b5f59999f6cbc07f1b3433cc
Ankit Wal <ankitwal@thoughtworks.com>
2022-09-14 16:55:15 +0800

    Refactor Tests

--- week3/stacks/app-a/tests/appa-a_test.go
+++ week3/stacks/app-a/tests/appa-a_test.go
@@ -13,7 +13,7 @@ var clusterContext string
 
 // TestClusterRDSIntegration is a quick and dirty but effective test that validates if a test pod can reach and log into the RDS.
 // This test relies on the k8s config and context to be updated correctly externally to the test.
-func TestClusterRDSIntegration(t *testing.T) {
+func TestAppAStack(t *testing.T) {
 	TeamName := os.Getenv("TEAM_NAME")
 	Environment := os.Getenv("ENVIRONMENT")
 	// namespace is hardcoded as a sort of convention between the underlying infra and the application layer.
@@ -33,22 +33,23 @@ func TestClusterRDSIntegration(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-
 	// WARNING - this updates the kubectl context for the whole machine
 	UpdateKubeConfig(t, clusterName, clusterRole)
 
-	// Create test fixtures
-	options := k8s.NewKubectlOptions("", "", namespace)
-	podYmlBytes, err := ioutil.ReadFile("./pod.yml")
-	if err != nil {
-		t.Error(err)
-	}
-	k8s.KubectlApplyFromString(t, options, string(podYmlBytes))
-	defer k8s.KubectlDeleteFromString(t, options, string(podYmlBytes))
-	k8s.WaitUntilPodAvailable(t, options, "test-pod", 15, 3*time.Second)
+	t.Run("AppShouldBeAbleToConnectWithRDS", func(t *testing.T) {
+		// Create test fixtures
+		options := k8s.NewKubectlOptions("", "", namespace)
+		podYmlBytes, err := ioutil.ReadFile("./pod.yml")
+		if err != nil {
+			t.Error(err)
+		}
+		k8s.KubectlApplyFromString(t, options, string(podYmlBytes))
+		defer k8s.KubectlDeleteFromString(t, options, string(podYmlBytes))
+		k8s.WaitUntilPodAvailable(t, options, "test-pod", 15, 3*time.Second)
 
-	// fire test command from test pod
-	// this will test if the test pod can reach the RDS DB at the given endpoint and port
-	// it will test if the test pod can log into the RDS DB with the given username and password
-	k8s.RunKubectl(t, options, []string{"exec", "test-pod", "--", "sh", "-c", "PGPASSWORD=$db_password psql -h $db_endpoint -d postgres -U $db_username"}...)
+		// fire test command from test pod
+		// this will test if the test pod can reach the RDS DB at the given endpoint and port
+		// it will test if the test pod can log into the RDS DB with the given username and password
+		k8s.RunKubectl(t, options, []string{"exec", "test-pod", "--", "sh", "-c", "PGPASSWORD=$db_password psql -h $db_endpoint -d postgres -U $db_username"}...)
+	})
 }
